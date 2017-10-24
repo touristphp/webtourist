4d12c7f8acb8bf5e5724251c9d63f077761f42d2
QuyItEngineer <hoangbuingocquy@gmail.com>
2017-10-24 23:37:26 +0700

    change folder and finish home page

---
+++ fontend/top/footer.php
@@ -0,0 +1,41 @@
+<!-- START FOOTER -->
+<div class="footer">
+    <div class="container">
+        <div class="row">                   
+            <div class="col-xs-12 text-center">
+                <div class="copyright">
+                    <p>Copyright &copy; 2017 |  All Rights Reserved.</p>
+                </div><!--- END FOOTER COPYRIGHT -->
+            </div><!--- END COL -->         
+        </div><!--- END ROW -->             
+    </div><!--- END CONTAINER -->
+</div>
+<!-- END FOOTER -->     
+
+<!-- Latest jQuery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- Latest compiled and minified Bootstrap -->
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- modernizer JS -->      
+<script src="assets/js/modernizr.2.5.3.min.js"></script>                                                                    
+<!-- owl-carousel min js  -->
+<script src="assets/owlcarousel/js/owl.carousel.min.js"></script>
+<!-- stellar js -->
+<script src="assets/js/jquery.stellar.min.js"></script>
+<!-- countTo js -->
+<script src="assets/js/jquery.inview.min.js"></script>  
+<!-- jquery flexslider min js -->
+<script src="assets/js/jquery.flexisel.js"></script>
+<script src="assets/js/jquery.flexslider-min.js"></script>              
+<!-- scrolltopcontrol js -->
+<script src="assets/js/scrolltopcontrol.js"></script>                                   
+<!-- form-contact js -->
+<script src="assets/js/form-contact.js"></script>   
+<!-- aos js -->
+<script src="assets/js/aos.js"></script>
+<!-- map js -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwIQh7LGryQdDDi-A603lR8NqiF3R_ycA"></script>             
+<!-- scripts js -->
+<script src="assets/js/scripts.js"></script>
+</body>
+</html>
