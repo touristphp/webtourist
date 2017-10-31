956c8fe4d91d66439b60d210ea1b556092110026
QuyItEngineer <hoangbuingocquy@gmail.com>
2017-10-31 20:14:53 +0700

    finish 1st page review.php

---
+++ fontend/top/review.php
@@ -0,0 +1,108 @@
+<?php include_once 'header.php';?>
+
+<!-- START TITLE TOP -->
+<section class="section-content section-padding" style="background-image: url(assets/img/bg/partner-bg.jpg);  background-size:cover; background-position: center center;">
+	<!-- <div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h1 class="section-blog-title">Single blog</h1>
+			</div>
+		</div>
+	</div> -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-left">
+				<a href="" class="section-blog-title">
+					<img src="assets/img/blog/c1.jpg" alt="">
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+<div>
+	<div class="container our_review">
+		<nav class="nav menu-review">
+			<div class="row">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					
+				</div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+					<a class="nav-link active button-review" href="#">Timeline</a>
+				</div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+					<a class="nav-link button-review" href="#">About</a>
+				</div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+					<a class="nav-link button-review" href="#">Photo</a>
+				</div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+					<a class="nav-link button-review " href="#">More</a>
+				</div>
+			</div>
+			
+			
+			
+			
+		</nav>
+	</div>
+</div>
+
+<!-- END TITLE TOP -->
+
+<!-- START ICON USER -->
+
+<div class="blog_page section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" data-aos="fade-up" >
+				<div class="blog_content">
+					
+					<h1 class="title">creativity is intelligence having fun</h1>
+					
+					<img src="assets/img/blog/1.jpg" class="img-responsive" alt="image">
+					<p>Lorem ipsum dosectetur adipisicing elit, sed doLorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. Mauris elementum accumsan leo vel tempo Sit amet cursus nisl aliquam. Aliquam et elit eu nunc rhoncus viverra quis at felis. Seddo Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus Lorem ipsum dosectetur a dipisicing elit at leo dignissim congue.</p>
+
+					<h4  data-aos="fade-up"><span class="date">25.07.2017</span> <span><i class="material-icons">thumb_up</i> 117 Likes </span></h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="blog_page section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" data-aos="fade-up">
+				<div class="blog_content">
+					<h1 class="title">creativity is intelligence having fun</h1>
+					
+					<img src="assets/img/blog/2.jpg" class="img-responsive" alt="image">
+					<p>Lorem ipsum dosectetur adipisicing elit, sed doLorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. Mauris elementum accumsan leo vel tempo Sit amet cursus nisl aliquam. Aliquam et elit eu nunc rhoncus viverra quis at felis. Seddo Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus Lorem ipsum dosectetur a dipisicing elit at leo dignissim congue.</p>
+
+					<h4 data-aos="fade-up"><span class="date">25.07.2017</span> <span><i class="material-icons">thumb_up</i> 17 Likes </span></h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="blog_page section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" data-aos="fade-up">
+				<div class="blog_content" >
+					<h1 class="title">creativity is intelligence having fun</h1>
+					
+					<img src="assets/img/blog/3.jpg" class="img-responsive" alt="image">
+					<p> Lorem ipsum dosectetur adipisicing elit, sed doLorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. Mauris elementum accumsan leo vel tempo Sit amet cursus nisl aliquam. Aliquam et elit eu nunc rhoncus viverra quis at felis. Seddo Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus Lorem ipsum dosectetur a dipisicing elit at leo dignissim congue.</p>
+
+					<h4 data-aos="fade-up"><span class="date">25.07.2017</span> <span><i class="material-icons">thumb_up</i> 77 Likes </span></h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="map"></div>
+
+
+<?php include_once 'footer.php'; ?>
